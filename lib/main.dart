fad73e8bb1c1f6e09a37306c4d2c7bc1b198e0cf
jyholic1209 <jyholic1209@gmail.com>
2022-10-21 18:48:13 +0900

    add Interactive button

--- lib/main.dart
+++ lib/main.dart
@@ -94,6 +94,7 @@ class MyApp extends StatelessWidget {
             titleSection,
             buttonSection,
             textSection,
+            TapboxA(),
           ],
         ),
       ),
@@ -167,6 +168,43 @@ class _FavoritWidgetState extends State<FavoritWidget> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  const TapboxA({super.key});
+
+  @override
+  State<TapboxA> createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
